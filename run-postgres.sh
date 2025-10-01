425af354094b3817dab8e1d44163f44dff9e54e1
ivan <lostnemo@rambler.ru>
2025-10-01 16:02:34 +0300

    Добавляет shell скрипт

--- run-postgres.sh
+++ run-postgres.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-docker run --name pg -e POSTGRES_PASSWORD=pass -v "$1":/data/pg -d postgres
+
+# 1. Запуск контейнера PostgreSQL
+docker run --name pg -e POSTGRES_PASSWORD=pass -v /home/vasa/data:/data/pg -d postgres
+
+# 2. Остановить и удалить контейнер
+docker stop pg
+docker rm pg
+
+# 3. Посмотреть содержимое каталога
+ls /home/vasa/data
+
+# 4. Перенести каталог
+mv /home/vasa/data /home/vasa/newdir/data
+
+# 5. Снова запустить контейнер с новым путем
+docker run --name pg -e POSTGRES_PASSWORD=pass -v /home/vasa/newdir/data:/data/pg -d postgres
